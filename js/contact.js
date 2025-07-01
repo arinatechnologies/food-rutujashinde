000b1919e9efde27309a84b8c65dd706ecb00f2b
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 07:43:27 -0500

    Upload from Lambda

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+{
+  "contact_images": [
+    "modern restaurant interior with warm lighting",
+    "chef preparing food in professional kitchen",
+    "group of people enjoying meal together",
+    "close-up of gourmet dish presentation",
+    "coffee shop interior with cozy atmosphere"
+  ]
+}
