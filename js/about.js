f3bfb6455fafcb6cd3a9b9ed935a4e86657b7366
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 07:44:18 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,12 @@
+[
+  "coffee shop interior with rustic decor",
+  "coffee beans being roasted",
+  "barista preparing coffee",
+  "group of people enjoying coffee in café",
+  "coffee farm with green coffee plants",
+  "coffee cup on wooden table",
+  "team of baristas working together",
+  "coffee brewing equipment closeup",
+  "coffee shop exterior with outdoor seating",
+  "hands holding coffee beans"
+]
